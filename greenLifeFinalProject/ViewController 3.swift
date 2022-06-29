169dc60e4bccebaaba70d853981a588142335459
Shivani LaBore <laboreshivani@gmail.com>
2022-06-29 14:15:52 -0500

    Tried doing constraints

---
+++ greenLifeFinalProject/ViewController 3.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  greenLifeFinalProject
+//
+//  Created by Lara Urteaga on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
