d226a8a5e937b3990f14be5cdbeab77845c0203d
Lara Urteaga <urteagalm@gmail.com>
2022-06-29 23:54:17 -0600

    Products

---
+++ greenLifeFinalProject/ProductOptionsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProductOptionsViewController.swift
+//  greenLifeFinalProject
+//
+//  Created by Lara Urteaga on 6/29/22.
+//
+
+import UIKit
+
+class ProductOptionsViewController: UIViewController {
+    
+    @IBOutlet weak var productPrompt: UILabel!
+    
+    @IBOutlet weak var menuBarProductOptions: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
